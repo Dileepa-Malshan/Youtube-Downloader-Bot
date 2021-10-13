28141ba4fcd4883668fecd1d21d52ad14681c111
Dileepa_Malshan <84204637+Dileepa-Malshan@users.noreply.github.com>
2021-10-13 13:17:21 +0545

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import  os
 
-BOT_TOKEN = os.environ.get("2075362091:AAHvNbnR-XLTcxkppIiZ0CJvS5sLQn5NRqY")
+BOT_TOKEN = os.environ.get("1975609518:AAE5zBIanju6x3x6PA7RqkjWEwGBMR9ZApg")
 APP_ID = int(os.environ.get("2405845"))
 API_HASH = os.environ.get("e4989b57060fe798957a1a40049ec3c2")
 
